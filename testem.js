5680a77c09a0b2e4f8c5e5fe7d6e5d9f2716b0c0
Brandon Drake <tomogiusuru@gmail.com>
2017-09-05 21:27:32 -0600

    Updated testem to use Chrome

--- testem.js
+++ testem.js
@@ -1,12 +1,15 @@
 /* eslint-env node */
 module.exports = {
-  test_page: 'tests/index.html?hidepassed',
-  disable_watching: true,
-  launch_in_ci: [
-    'PhantomJS'
+  "test_page": "tests/index.html?hidepassed",
+  "disable_watching": true,
+  "launch_in_ci": [
+    "Chrome"
   ],
-  launch_in_dev: [
-    'PhantomJS',
-    'Chrome'
-  ]
+  "launch_in_dev": [
+    //"PhantomJS",
+    "Chrome"
+  ],
+  "browser_args": {
+    'Chrome': [ '--headless', '--disable-gpu', '--remote-debugging-port=9222' ]
+  }
 };
